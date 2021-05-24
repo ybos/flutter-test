a68fa75243d0ad960360f66e4d5829757e8a66aa
Joker Huang <ybo.sama@gmail.com>
2021-05-25 00:25:38 +0800

    test helloworld app. only text component.

---
+++ lib/main_hello_world.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new Center(
+    child: Text(
+      "Hello, world!",
+      textDirection: TextDirection.ltr
+    )
+  ));
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_study/main.dart';
+import 'package:flutter_study/main_hello_world.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
