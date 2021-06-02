5fbca8d08afceb3f8c8727df704700386c1359e1
Joker Huang <ybo.sama@gmail.com>
2021-06-02 22:50:13 +0800

    build items on demand

---
+++ lib/list_map_data.dart
@@ -0,0 +1,72 @@
+import "package:flutter/material.dart";
+
+var listData = [
+  {
+    "title": "一文看懂华为发布会：万物皆可连接的HarmonyOS来了！",
+    "image": "https://cms-bucket.ws.126.net/2021/0602/aa36975bp00qu2uu6001nc0009c0070c.png?imageView&thumbnail=190y120"
+  },
+  {
+    "title": "英伟达CEO：我对中英监管机构批准收购Arm交易充满信心",
+    "image": "https://cms-bucket.ws.126.net/2021/0602/061cc64dp00qu2f10003yc0009c0070c.png?imageView&thumbnail=190y120"
+  },
+  {
+    "title": "台积电CEO：120亿美元亚利桑那州芯片厂已开工建设",
+    "image": "https://cms-bucket.ws.126.net/2021/0602/3cdc1c33p00qu1qib004xc0009c0070c.png?imageView&thumbnail=190y120"
+  },
+  {
+    "title": "沈向洋、王海峰候选中国工程院院士！计算机领域7位入围增选",
+    "image": "https://cms-bucket.ws.126.net/2021/0602/c8a218c2p00qu2oj000byc0009c0070c.png?imageView&thumbnail=190y120"
+  },
+  {
+    "title": "苏宁签署股权转让协议 向新零售基金转让总股本的5.59%",
+    "image": "https://cms-bucket.ws.126.net/2021/0602/e341c275p00qu2sdd003tc0009c0070c.png?imageView&thumbnail=190y120"
+  },
+  {
+    "title": "亚马逊Prime会员日开启，中国消费者享21场定制折扣",
+    "image": "https://cms-bucket.ws.126.net/2021/0602/86039858p00qu2sgh003oc0009c0070c.png?imageView&thumbnail=190y120"
+  }
+];
+
+void main() {
+  runApp(ContainerListMapData());
+}
+
+class ContainerListMapData extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: DefaultContainer(),
+      )
+    );
+  }
+}
+
+class DefaultContainer extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ListMapData();
+  }
+}
+
+/**
+ * 这里需要注意, ListView 和 ListTile 都需要一个 Material Widget 的 ancestor
+ * 所以需要在 MaterialApp 内调用 Scaffold
+ */
+class ListMapData extends State<DefaultContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.separated(
+          itemBuilder: (_, index) {
+            return ListTile(
+              leading: Image.network(listData[index]["image"]),
+              title: Text(listData[index]["title"]),
+            );
+          },
+          itemCount: listData.length,
+          separatorBuilder: (_, __) => Divider(),
+      )
+    );
+  }
+}
