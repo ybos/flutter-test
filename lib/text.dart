72aaf00ec48d96e28baead07b1cc8185800b855a
Joker Huang <ybo.sama@gmail.com>
2021-06-06 23:57:11 +0800

    test overflow | textAlign widget.

--- lib/text.dart
+++ lib/text.dart
@@ -1,7 +1,16 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(new TextTestWidget());
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text("Testing Text"),
+          leading: Icon(Icons.home),
+          centerTitle: true,
+        ),
+        body: TextTestWidget()
+    ))
+  );
 }
 
 /**
@@ -12,18 +21,40 @@ void main() {
 class TextTestWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "test",
-        // 这是必须键入的属性, 否则将会告诉你无法知道文字的方向性
-        textDirection: TextDirection.ltr,
-        // 这里可以通过 TextStyle 的属性来描述文字的样式
-        style: TextStyle(
-          fontSize: 20.0,
-          color: Colors.orange,
-          decoration: TextDecoration.underline
+    return Column(
+      children: <Widget>[
+        Text(
+          "style:TextStyle",
+          // 这是必须键入的属性, 否则将会告诉你无法知道文字的方向性
+          textDirection: TextDirection.ltr,
+          // 这里可以通过 TextStyle 的属性来描述文字的样式
+          style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.orange,
+              decoration: TextDecoration.underline
+          ),
+          textAlign: TextAlign.center,
         ),
-      )
+        SizedBox(height: 10),
+        Text(
+          "烤冷面，是黑龙江省鸡西市密山市特色小吃之一，烤冷面起初有多种做法，包括油炸、高温铁板碾压等，后改良为铁板成块煎烤。21世纪初烤冷面已经火遍全国的大江南北，是东北极具特色的美食。",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            decoration: TextDecoration.underline
+          ),
+          // 这里应该会显示 3 行, 指定 2 行的话, 多余的行数会被隐藏
+          maxLines: 2,
+        ),
+        SizedBox(height: 10),
+        Text(
+          "烤冷面，是黑龙江省鸡西市密山市特色小吃之一，烤冷面起初有多种做法，包括油炸、高温铁板碾压等，后改良为铁板成块煎烤。21世纪初烤冷面已经火遍全国的大江南北，是东北极具特色的美食。",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            color: Colors.blueAccent
+          ),
+          overflow: TextOverflow.ellipsis,
+        )
+      ]
     );
   }
 }
